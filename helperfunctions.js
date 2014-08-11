05ce7cac8911a7d3267568281c3bb956a2c87796
Michael Swartz <hydrokool@gmail.com>
2014-08-11 14:38:50 -0700

    Updating to support IE8

--- helperfunctions.js
+++ helperfunctions.js
@@ -1,15 +1,31 @@
-Handlebars.registerHelper('ifLastObj', function (v1, v2, options) {
+Handlebars.registerHelper('ifLastObj', function (indexVal, objArray, options) {
     /* This is to resolve the issue where Handlebars will not provide a method to know
        when you've reached the last item in an object.  They only do so for Array's with @last.
        Both Array and Objects can use @first though
+       
+       This now supports IE8 as IE8 cannot use ES5 .keys
     */
 
     var fn = options.fn, inverse = options.inverse;
-
-    if (v1 == (Object.keys(v2).length - 1)) {
-        return fn(this);
+//
+    if (Object.keys) {
+        if (indexVal == (Object.keys(objArray).length - 1)) {
+            return fn(this);
+        } else {
+            return inverse(this);
+        }
     } else {
-        return inverse(this);
+        var objArrayLength = 0;
+        $.each(objArray, function () {
+            objArrayLength++;
+        });
+
+        if ((objArrayLength - 1) > indexVal) {
+            return inverse(this);
+        } else {
+            objArrayLength = 0;
+            return fn(this);
+        }
     }
 
 });
